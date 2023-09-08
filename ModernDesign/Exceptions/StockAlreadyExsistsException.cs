08e921feb288b91e5e07a95c1e5ba10944c82288
RoanCreed7 <40430682@live.napier.ac.uk>
2023-09-08 13:08:02 +0100

    Duplicate data entry fixed. User can no longer enter the same stock on the addstockview

---
+++ ModernDesign/Exceptions/StockAlreadyExsistsException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ModernDesign.Exceptions
+{
+    internal class StockAlreadyExsistsException : Exception
+    {
+        public StockAlreadyExsistsException()
+        {
+            MessageBox.Show("Stock name already exsits. Please use the edit page to edit stocks.");
+        }
+
+        public StockAlreadyExsistsException(string message) : base(message)
+        {
+        }
+
+        public StockAlreadyExsistsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+    }
+}
